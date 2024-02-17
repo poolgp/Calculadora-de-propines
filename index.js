0ebe21909524bb6f1da488b4b59fe7972acb887e
poolgp <145465364+poolgp@users.noreply.github.com>
2024-02-17 13:32:08 +0100

    17/02/2024

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 function calcula() {
   let formulari = document.getElementById("idForm");
-  let NumComensals = formulari.inputComensals.value;
-  let TotalDiners = parseFloat(formulari.inputTotalDiners.value);
-  let Opinio = formulari.selectOpinio.value;
-  const minPropina  = 0.5;
+  let numComensales = formulari.inputComensales.value;
+  let totalDiners = formulari.inputTotalDiners.value;
+  totalDiners = parseFloat(totalDiners);
+  let opinion = formulari.selectOpinion.value;
+  var percentatge = 0;
+  const minPropina = 0.5;
 
-  if (NumComensals == " " || NumComensals < 1 || isNaN(NumComensals)) {
-    NumComensals = 1;
+  if (numComensales === " " || numComensales < 1 || isNaN(numComensales)) {
+    numComensales = 1;
   }
 
-  if (isNaN(TotalDiners) || TotalDiners < 0.01) {
+  if (isNaN(totalDiners) || totalDiners < 0.01) {
     alert("Introdueix un import valit: mínim 0.01€");
     return;
   }
 
-  if (Opinio === "escull") {
+  if (opinion === "escull") {
     alert("Opina sobre el servei: ");
     return;
   }
 
-  var percentatge = 0;
-  switch (Opinio) {
+  switch (opinion) {
     case "genial":
       percentatge = 0.1;
       break;
@@ -32,16 +33,21 @@ function calcula() {
       break;
   }
 
-  let propinaTotal = TotalDiners * percentatge;
-  let propinaPersona = propinaTotal / NumComensals;
+  let propinaTotal = totalDiners * percentatge;
+  let propinaPersona = propinaTotal / numComensales;
 
-  if (propinaPersona < 0.5) {
-    alert("La propina es menor a 0.5")
+  if (propinaPersona < minPropina) {
+    alert("La propina es menor a 0.5");
     propinaPersona = 0.5;
   }
 
   let resultadoElement = document.getElementById("resultadoPropina");
-  resultadoElement.textContent = "Propina total: " + propinaTotal.toFixed(2) + "€ | Propina por persona: " + propinaPersona.toFixed(2) + "€";
+  resultadoElement.textContent =
+    "Propina total: " +
+    propinaTotal.toFixed(2) +
+    "€ | Propina por persona: " +
+    propinaPersona.toFixed(2) +
+    "€";
 
-  formulario.reset();
+  formulari.reset();
 }
